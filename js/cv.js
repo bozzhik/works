b3fcc4e51076d1884f142c5f23eb744b96b28cac
max <bozzhik@ya.ru>
2023-01-09 00:37:57 +0300

    add showTextCards

--- js/cv.js
+++ js/cv.js
@@ -19,3 +19,42 @@ setTimeout(() => {
    entryScreen.style.display = 'block'
    entryScreen.style.animationPlayState = 'running'
 }, 3500)
+
+const works = document.getElementById('works')
+const github = document.getElementById('github')
+const find = document.getElementById('find')
+const resume = document.getElementById('resume')
+const titleWorks = document.getElementById('title-works')
+const titleGithub = document.getElementById('title-github')
+const titleFind = document.getElementById('title-find')
+const titleResume = document.getElementById('title-resume')
+
+function showTitleCards() {
+   works.addEventListener('mouseenter', function () {
+      titleWorks.style.opacity = 1
+      titleWorks.animationPlayState = 'running'
+   })
+   github.addEventListener('mouseenter', function () {
+      titleGithub.style.opacity = 1
+   })
+   find.addEventListener('mouseenter', function () {
+      titleFind.style.opacity = 1
+   })
+   resume.addEventListener('mouseenter', function () {
+      titleResume.style.opacity = 1
+   })
+   works.addEventListener('mouseleave', function () {
+      titleWorks.style.opacity = 0
+   })
+   github.addEventListener('mouseleave', function () {
+      titleGithub.style.opacity = 0
+   })
+   find.addEventListener('mouseleave', function () {
+      titleFind.style.opacity = 0
+   })
+   resume.addEventListener('mouseleave', function () {
+      titleResume.style.opacity = 0
+   })
+}
+
+showTitleCards()
