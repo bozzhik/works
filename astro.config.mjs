b079e08452dc306f5337fecd9c0f784819c5947f
max <bozzhik@ya.ru>
2023-09-09 02:25:37 +0300

    disable `vercel` deploying configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-import vercel from "@astrojs/vercel/serverless";
+import {defineConfig} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   server: {
     port: 2000,
-    open: true
+    open: true,
   },
-  output: "server",
-  adapter: vercel()
-});
+})
